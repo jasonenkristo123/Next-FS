c75121189d5a8d80ec0e015829e095ae9ff4cddb
jasonenkristo123 <jasonenkristo@gmail.com>
2026-06-12 20:51:57 +0700

    success access database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
+import { supabase } from "@/shared/lib/supabase";
 
 
-export default function Home() {
+export default async function Home() {
+  const { data, error } = await supabase.from("events").select("*");
+
+
+  if (error) {
+    console.log("data", data)
+    console.log("error", error)
+    return <div>Error: {error.message}</div>
+  }
   return (
    <main>
-    
+    {data?.map((event) => (
+      <div key={event.id}>
+        <h2>
+          {event.title}
+        </h2>
+        <p>
+          {event.description}
+        </p>
+
+      </div>
+    ))}
    </main>
   );
 }
